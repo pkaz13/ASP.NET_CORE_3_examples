1f86666caf9a5240eea8bdd464f1614ab0ecfee1
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-03 19:41:20 +0200

    Added project for data access & model

---
+++ Dapper/DataAccessLib/Model/EmailAddress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLib.Model
+{
+    public class EmailAddress
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public int PersonId { get; set; }
+    }
+}
