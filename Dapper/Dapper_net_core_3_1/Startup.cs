acdffa5277d75a6f3ad587193eceb2c51bd29b70
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-03 21:03:26 +0200

    Added PeopleDataAccess to DI container

--- Dapper/Dapper_net_core_3_1/Startup.cs
+++ Dapper/Dapper_net_core_3_1/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DataAccessLib;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Dapper_net_core_3_1
 {
@@ -26,6 +20,7 @@ namespace Dapper_net_core_3_1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IPeopleDataAccess, PeopleDataAccess>(x => new PeopleDataAccess(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
