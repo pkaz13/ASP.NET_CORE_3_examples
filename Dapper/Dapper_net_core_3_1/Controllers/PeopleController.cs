f611f6e58ebebe73a36a98e676e7e006f7f2e8b7
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-04-03 21:04:30 +0200

    Added PeopleController with GetAll & GetById endpoints

---
+++ Dapper/Dapper_net_core_3_1/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLib;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_net_core_3_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly IPeopleDataAccess _peopleDataAccess;
+
+        public PeopleController(IPeopleDataAccess peopleDataAccess)
+        {
+            _peopleDataAccess = peopleDataAccess;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_peopleDataAccess.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_peopleDataAccess.GetById(id));
+        }
+    }
+}
